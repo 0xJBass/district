a7a1ceab5e9baf196ecf04e2526ee24793914e3c
justin <justinbassey@gmail.com>
2023-05-24 22:45:05 -0400

    fix: website

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-2 justify-start p-24">
+      <h1>District Website</h1>
+    </main>
+  );
+}
